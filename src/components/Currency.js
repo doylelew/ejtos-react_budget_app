60ae9bc729284f8058de8708b3c890c86310a392
dingodoyle <doyle.lewis.a@gmail.com>
2023-08-07 16:12:04 -0400

    almost working dropbox

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,32 +1,24 @@
-import React, { useState } from 'react';
-// import { AppContext } from '../context/AppContext';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 import './Select.css';
 
 const Currency = () => {
-    const [open, setOpen] = useState(false)
 
-    // const { expenses } = useContext(AppContext);
+    const {dispatch, currency} = useContext(AppContext);
 
-    if (open){
-        return(
-            <div className = "dropdown">
-                <button className="select-button btn btn-lrg rounded-2" onClick={()=>setOpen(!open)} ><h5>Currency ($ Dollar) ▾</h5></button>
-                <table className="drop-options">
-                    <tr><option className="subbutton">$ Dollar</option></tr>
-                    <tr><option className="subbutton">£ Pound</option></tr>
-                    <tr><option className="subbutton">€ Euro</option></tr>
-                    <tr><option className="subbutton">₹ Ruppee</option></tr>
-                </table>
-            </div>
-        );
+    const eventHandler= (e)=>{
+        console.log(e.target.value)
+    };
 
-    } else {
-        return(
-            <div className = "dropdown">
-                <button className="select-button btn btn-lrg rounded-2" onClick={()=>setOpen(!open)} ><h5>Currency ($ Dollar) ▾</h5></button>
-            </div>
-        );
-    }
+    return(
+        <select name="Currency" id="Currency" onChange={eventHandler} value="default">
+            <option selected hidden value="defualt">Currency ({currency} Dollar)</option>
+            <option value="$">$ Dollar</option>
+            <option value="£">£ Pound</option>
+            <option value="€">€ Euro</option>
+            <option value="₹">₹ Ruppee</option>
+        </select>
+    );
 };
 
 export default Currency;
