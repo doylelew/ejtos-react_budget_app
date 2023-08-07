bf0151374806119b27caeef002123978573f607a
dingodoyle <doyle.lewis.a@gmail.com>
2023-08-07 17:16:08 -0400

    Working currency record change

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -7,12 +7,35 @@ const Currency = () => {
     const {dispatch, currency} = useContext(AppContext);
 
     const eventHandler= (e)=>{
-        console.log(e.target.value)
+        dispatch({
+            type:'CHG_CURRENCY',
+            payload: e.target.value
+        });
     };
 
+    function getCurrencyName(currency){
+        switch(currency){
+            case '$':
+                return "Dollar"
+            case '£':
+                return "Pound"
+            case '€':
+                return "Euro"
+            case '₹':
+                return "Ruppee"
+            
+            default:
+                return null;
+
+
+        }
+    }
+
+    const currencyType = getCurrencyName(currency)
+
     return(
-        <select name="Currency" id="Currency" onChange={eventHandler} value="default">
-            <option selected hidden value="defualt">Currency ({currency} Dollar)</option>
+        <select className="select-box" name="Currency" id="Currency" onChange={eventHandler} value="default">
+            <option selected hidden value="defualt">Currency ({currency} {currencyType})</option>
             <option value="$">$ Dollar</option>
             <option value="£">£ Pound</option>
             <option value="€">€ Euro</option>
